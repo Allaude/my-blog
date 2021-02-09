b8d57fa4876716314ba3e7346378bba322b8a7b7
Muhamad Riyan <riyan@skyshi.com>
2021-02-09 22:20:29 +0700

    change grid value

--- src/components/templates/BlogHomeTemplate/BlogHomeTemplate.tsx
+++ src/components/templates/BlogHomeTemplate/BlogHomeTemplate.tsx
@@ -22,7 +22,7 @@ export default function BlogHomePage({
         <Head>
           <title>Blog | Muhamad Riyan</title>
         </Head>
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <div className="p-10 grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5">
           {allPosts.map((post) => (
             <PostPreview post={post} key={post.title} />
           ))}
